f5f40c4c6ae3ec49c1a4a976f9d9fe1c0fb07563
Samuel Keer <sirpizzalot@gmail.com>
2019-05-03 18:39:01 -0500

    implemented simple nav bar for routing

---
+++ src/app/components/business-listings/business-listings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-listings',
+  templateUrl: './business-listings.component.html',
+  styleUrls: ['./business-listings.component.css']
+})
+export class BusinessListingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
