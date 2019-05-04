0ecefe82f68db2da19f8a9a073ace91f4e19d85b
Winntana Beyene <winntanab@gmail.com>
2019-05-03 20:02:54 -0500

    camera working , taking snapshots and saving to screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { HomeViewComponent } from './components/home-view/home-view.component';
 import { BusinessListingsComponent } from './components/business-listings/business-listings.component';
+import { IsThisBlackOwnedComponent } from './components/is-this-black-owned/is-this-black-owned.component';
+import {WebcamModule} from 'ngx-webcam';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BusinessListingsComponent,
-    HomeViewComponent
+    HomeViewComponent,
+    IsThisBlackOwnedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebcamModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
