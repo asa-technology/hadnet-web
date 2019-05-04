d70282550d33d62b0f3a7ced5ab6103c2e31b4c3
Adam Hebert <adam.hebert656@gmail.com>
2019-05-03 20:31:56 -0500

    Started making login component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeViewComponent } from './components/home-view/home-view.component';
 import { BusinessListingsComponent } from './components/business-listings/business-listings.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeViewComponent},
-  {path: 'business-listings', component: BusinessListingsComponent}
+  {path: 'business-listings', component: BusinessListingsComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { HomeViewComponent } from './components/home-view/home-view.component';
 import { BusinessListingsComponent } from './components/business-listings/business-listings.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BusinessListingsComponent,
-    HomeViewComponent
+    HomeViewComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
