370d30d9285a19347cbcf58d40d4151bd32390f7
Samuel Keer <sirpizzalot@gmail.com>
2019-05-04 11:45:49 -0500

    implemented map view and added a marker representing a fake business

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare let L;
+declare let tomtom: any;
 
 @Component({
   selector: 'app-map-view',
@@ -10,6 +12,15 @@ export class MapViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const map = tomtom.L.map('map', {
+      key: 'BATuQkjG9LX7IGcAzxbZVkXG1GUPsF68',
+      basePath: '/assets/sdk',
+      center: [ 30, -91 ],
+      zoom: 15,
+      source : 'vector'
+    });
+    const marker: any = tomtom.L.marker([29.9511, -90.0715]).addTo(map);
+    marker.bindPopup('hadnet headquarters', 'address').openPopup();
   }
 
 }
