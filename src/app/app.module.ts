c990790270994e15fefdae617fe9916c9d62a254
Adam Hebert <adam.hebert656@gmail.com>
2019-05-04 09:50:31 -0500

    Merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { HomeViewComponent } from './components/home-view/home-view.component';
 import { BusinessListingsComponent } from './components/business-listings/business-listings.component';
-<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
-=======
 import { IsThisBlackOwnedComponent } from './components/is-this-black-owned/is-this-black-owned.component';
 import {WebcamModule} from 'ngx-webcam';
 import {FormsModule} from '@angular/forms';
->>>>>>> db3b343944961982af302b4780d8bb24b2a1750b
 import { BusinessListingItemComponent } from './components/business-listing-item/business-listing-item.component';
 
 
@@ -30,23 +27,17 @@ import { BusinessListingItemComponent } from './components/business-listing-item
     HeaderComponent,
     BusinessListingsComponent,
     HomeViewComponent,
-<<<<<<< HEAD
     LoginComponent,
-=======
     IsThisBlackOwnedComponent,
->>>>>>> db3b343944961982af302b4780d8bb24b2a1750b
     BusinessListingItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-=======
     WebcamModule,
     FormsModule,
->>>>>>> db3b343944961982af302b4780d8bb24b2a1750b
     HttpClientModule
   ],
   providers: [],
