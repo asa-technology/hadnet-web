7bd312e34fceb63a6aca45800eb72156875e6a58
Samuel Keer <sirpizzalot@gmail.com>
2019-05-04 12:29:22 -0500

    business names rendering via mock data

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BusinessListingService } from 'src/app/services/business-listings/business-listing.service';
 declare let L;
 declare let tomtom: any;
 
@@ -8,8 +9,8 @@ declare let tomtom: any;
   styleUrls: ['./map-view.component.css']
 })
 export class MapViewComponent implements OnInit {
-
-  constructor() { }
+  // gets all business listings for rendering on map
+  constructor(private businessListingService: BusinessListingService) { }
 
   ngOnInit() {
     const map = tomtom.L.map('map', {
@@ -19,6 +20,11 @@ export class MapViewComponent implements OnInit {
       zoom: 15,
       source : 'vector'
     });
+    this.businessListingService.getBusinessListings().subscribe( (businessListings) => businessListings.forEach((listing) => {
+      const businessMarker: any = tomtom.L.marker([listing.latitude, listing.longitude]).addTo(map);
+      businessMarker.bindPopup(`<strong>${listing.name}</strong><br>${listing.address}`);
+      console.log(listing);
+    }));
     const marker: any = tomtom.L.marker([29.9511, -90.0715]).addTo(map);
     marker.bindPopup('hadnet headquarters', 'address').openPopup();
   }
