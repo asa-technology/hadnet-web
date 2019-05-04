719a5e0d1b613ac6d90d479378b1de329f5d1f28
Adam Hebert <adam.hebert656@gmail.com>
2019-05-03 19:58:41 -0500

    Firebase set up.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+
+/**
+ * Firebase Setup
+ */
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,9 +23,12 @@ import { BusinessListingsComponent } from './components/business-listings/busine
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
