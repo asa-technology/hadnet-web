68f66f79666d08297954e9f48ee040fe62ee2bbb
Jordan Estes <jordan.estes@gmail.com>
2019-05-06 19:11:29 -0500

    added more endpoints

--- database/helpers.js
+++ database/helpers.js
@@ -13,16 +13,23 @@ const  {
 const addBusiness = (businessObj) => {
     return Business.create(businessObj)
     .then((result) => {
-        console.log('entered into db');
+        console.log('entered business into db');
         return result})
-    .catch(err => err);
+    .catch((err) => {
+        console.log(err)
+    });
 };
 
+
+// Get all businesses
 const getAllBusinesses = () => {
     return Business.findAll()
     .then((results) => {
         return results;
     })
+    .catch((err) => {
+        console.log(err)
+    });
 }
 
 // Get business by id
@@ -35,10 +42,62 @@ const getBusinessById = (id) => {
     .then((business) => {
         return business;
     })
+    .catch((err) => {
+        console.log(err)
+    });
+}
+
+ // Add user to database
+ const addUser = (userObj) => {
+    return User.create(userObj)
+    .then((result) => {
+        console.log('entered user into db');
+        return result})
+    .catch((err) => {
+        console.log(err)
+    });
+};
+
+// Get user by firebase id
+const getUserById = (id) => {
+    return User.findOne({
+        where: {
+            firebase_id: id,
+        }
+    }).then((user) => {
+        return user;
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+}
+
+// add review
+const addReview = (reviewObj) => {
+    return Review.create(reviewObj)
+    .then((result) => {
+        return result;
+    })
+    .catch((err) => {
+        console.log(err)
+    });  
+}
+
+const getReviewsByBusiness = (businessId) => {
+    return Review.findAll({
+        where: {
+            id_business: businessId,
+        }
+    })
 }
 
 module.exports = {
     addBusiness,
     getBusinessById,
     getAllBusinesses,
+    addUser,
+    getUserById,
+    addReview,
+    getReviewsByBusiness,
+
 }
