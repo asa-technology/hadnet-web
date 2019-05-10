4b7c4263b24aa62efa115134da53811652eef00d
Winntana Beyene <winntanab@gmail.com>
2019-05-09 20:04:21 -0500

    community board created

---
+++ src/app/components/community-board/community-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-board',
+  templateUrl: './community-board.component.html',
+  styleUrls: ['./community-board.component.css']
+})
+export class CommunityBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
