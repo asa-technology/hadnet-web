aea969769dcc9200047d49dbab785ee5d26e8e03
Adam Hebert <adam.hebert656@gmail.com>
2019-05-11 10:53:02 -0500

    Rating pipe works, edit forms work.

--- src/app/pipes/rating.pipe.ts
+++ src/app/pipes/rating.pipe.ts
@@ -3,19 +3,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'rating'})
 export class RatingPipe implements PipeTransform {
-  transform(rating: number): string {
-    switch(rating) {
-      case 1:
+  transform(rating): string {
+    switch (rating) {
+      case '1':
         return '&#9733;&#9734;&#9734;&#9734;&#9734;';
-      case 2:
+      case '2':
         return '&#9733;&#9733;&#9734;&#9734;&#9734;';
-      case 3:
+      case '3':
         return '&#9733;&#9733;&#9733;&#9734;&#9734;';
-      case 4:
+      case '4':
         return '&#9733;&#9733;&#9733;&#9733;&#9734;';
-      case 5:
-      case 0:
+      case '5':
         return '&#9733;&#9733;&#9733;&#9733;&#9733;';
+      case '0':
+        return 'No ratings yet...';
     }
   }
 }
