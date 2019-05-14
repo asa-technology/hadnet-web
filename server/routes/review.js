397544892cc62b0efed25ceebecffc989f28905a
Adam Hebert <adam.hebert656@gmail.com>
2019-05-14 13:57:45 -0500

    Documented review endpoints.

--- server/routes/review.js
+++ server/routes/review.js
@@ -7,27 +7,46 @@ const {
   getReviewsByUser,
 } = require('../../database/helpers');
 
-
+/**
+ * Grabs all reviews associated with a specified business.
+ * @name Get Reviews By Business ID
+ * @route {GET} /api/review/business/:id
+ * @routeparam {Number} id is the unique identifier for a business.
+ */
 router.get('/business/:id', (req, res) => {
   const { id } = req.params;
   getReviewsByBusiness(id)
     .then((reviews) => {
       res.send(reviews);
     }).catch((err) => {
+      console.error(err);
       res.sendStatus(404);
     });
 });
 
+/**
+ * Grabs all reviews associated with a specified user (specified by ID).
+ * @name Get Reviews By User ID
+ * @route {GET} /api/review/user/:id
+ * @routeparam {Number} id is the unique identifier for a user.
+ */
 router.get('/user/:id', (req,res) => {
   const userId = parseInt(req.params.id, 10);
   getReviewsByUser(userId)
     .then((reviews) => {
       res.send(reviews);
     }).catch((err) => {
+      console.error(err);
       res.sendStatus(404);
     });
 });
 
+/**
+ * Adds a review to the database.
+ * @name Add Review
+ * @route /api/review/
+ * @bodyparam {Object} review is an object containing the info for a review.
+ */
 router.post('/', (req, res) => {
   const review = req.body;
   const { idUser } = review;
@@ -45,8 +64,6 @@ router.post('/', (req, res) => {
           res.send(result);
         });
     });
-  /** **************TODO****************
-   * add review to specified business
-   */
 });
+
 module.exports = router;
