9419001345072775708fb6efb6e5166ea81f3331
Jordan Estes <jordan.estes@gmail.com>
2019-05-15 18:53:59 -0500

    documented formatAddress

--- src/app/pipes/formatAddress.pipe.ts
+++ src/app/pipes/formatAddress.pipe.ts
@@ -1,8 +1,22 @@
+/**
+ * This is a pipe to format addresses to two lines.
+ */
+
+/**
+ * import Pipe and PipeTransform from @angular/core.
+ */
 import { Pipe, PipeTransform } from '@angular/core';
 
-
 @Pipe({name: 'formatAddress'})
+/**
+ * FormatAddressPipe is a pipe for formatting an address.
+ */
 export class FormatAddressPipe implements PipeTransform {
+/**
+ * 
+ * @param address address being passed into the pipe.
+ * @returns a string with the address formatted.
+ */
   transform(address: string): string {
     let formatedAdd;
     if (address.includes(' New ')) {
