fff0a30ca103f0b722dffdd03070b292da6dbb29
Adam Hebert <adam.hebert656@gmail.com>
2019-05-14 20:13:20 -0500

    Fixed merge

--- server/routes/businesses.js
+++ server/routes/businesses.js
@@ -177,7 +177,6 @@ router.put('/update/:id', (req, res) => {
     .catch(error => console.error(error));
 });
 
-<<<<<<< HEAD
 /**
  * Verifies a business by checking if text in an image matches any business name in the database.
  * @name Verify A Business
@@ -185,11 +184,8 @@ router.put('/update/:id', (req, res) => {
  * @bodyparam {String} img is the base64 encoded image to be checked for text.
  * @memberof BusinessRoutes
  */
-router.post('/isVerfied', (req, res) => {
-=======
 // verifies a business by checking if image data name matches any buisness name in the table
 router.post('/isVerified', (req, res) => {
->>>>>>> 30cc6dda16f79fc2685d714c77e9de0e1f8c5c27
   axios.post(`https://vision.googleapis.com/v1/images:annotate?key=${process.env.GOOGLE_IMAGE_VERIFY_KEY}`, {
     requests: [
       {
