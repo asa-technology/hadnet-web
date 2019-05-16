1e149ca72ff8ed134417d6d4ab6ae3977b00871d
Adam Hebert <adam.hebert656@gmail.com>
2019-05-16 09:45:55 -0500

    Documented AuthService.

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,3 +1,7 @@
+/**
+ * This service handles all auth and user info.
+ * @namespace AuthService
+ */
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
@@ -8,10 +12,30 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * AuthService is a class for handling all auth related operations.
+ * @class AuthService
+ */
 export class AuthService {
+  /**
+   * user contains all Firebase related user information.
+   * This is only for use with Firebase related operations.
+   */
   user: User;
+  /**
+   * localUser contains all the user information stored in Hadnet's database.
+   * This should be used most of the time when dealing with user info.
+   */
   localUser;
+  /**
+   * usersBusiness is the business that is claimed by the logged in user.
+   * This is used to bring up that business's profile when needed.
+   */
   usersBusiness = null;
+  /**
+   * In the constructor, we attempt to initialize the user through Firebase
+   * And then using the Firebase UID, we find the user in the database and set localUser.
+   */
   constructor(public afAuth: AngularFireAuth, public router: Router, public http: HttpClient) {
     this.afAuth.authState.subscribe(user => {
       if (user) {
@@ -25,6 +49,12 @@ export class AuthService {
     });
   }
 
+  /**
+   * Attempts to sign up a new user with the information they provided.
+   * @param email A string representing the email the user is attempting to sign up with.
+   * @param password A string representing the password the user is attempting to sign up with.
+   * @param displayName A string representing the displayName the user is attempting to sign up with.
+   */
   async signup(email: string, password: string, displayName: string) {
     try {
       await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
@@ -50,6 +80,11 @@ export class AuthService {
     }
   }
 
+  /**
+   * Attempts to log in the user using the information they provided.
+   * @param email A string representing an email the user is attempting to log in with.
+   * @param password A string representing a password the user is attempting to log in with.
+   */
   async login(email: string, password: string) {
     try {
       await this.afAuth.auth.signInWithEmailAndPassword(email, password);
@@ -61,6 +96,9 @@ export class AuthService {
     }
   }
 
+  /**
+   * Logs out the currently logged in user.
+   */
   async logout() {
     await this.afAuth.auth.signOut();
     localStorage.removeItem('user');
@@ -69,6 +107,9 @@ export class AuthService {
     this.router.navigate(['login']);
   }
 
+  /**
+   * Logs in the user with their Facebook information.
+   */
   async fbLogin() {
     try {
       const provider = new firebase.auth.FacebookAuthProvider();
@@ -89,33 +130,60 @@ export class AuthService {
     }
   }
 
+  /**
+   * Returns whether the user is logged in or not.
+   * @return A boolean representing whether the user is logged in or not.
+   */
   get isLoggedIn(): boolean {
     const user = JSON.parse(localStorage.getItem('user'));
     return user !== null;
   }
 
+  /**
+   * Returns the currently logged in user's Firebase info.
+   * @return A Firebase User object.
+   */
   get currentUser() {
     return this.user;
   }
 
+  /**
+   * Returns the currently logged in user's local info.
+   * @return A user's local information object.
+   */
   get currentLocalUser() {
     return this.localUser;
   }
 
+  /**
+   * Returns the currently logged in user's claimed business.
+   * @return A user's claimed business.
+   */
   get currentUsersBusiness() {
     return this.usersBusiness;
   }
 
+  /**
+   * Attempts to reset a user's business.
+   */
   refreshUserBusiness() {
     return this.http.get(`/api/business/firebaseId/${this.user.uid}`).subscribe(business => {
       this.usersBusiness = business;
     });
   }
 
+  /**
+   * Returns whether or not the currently logged in user has claimed a business.
+   * @return A boolean representing whether the user has a claimed business.
+   */
   hasBusiness() {
     return !!this.usersBusiness;
   }
 
+  /**
+   * Returns whether or not the currently logged in user can claim a business.
+   * @return A boolean representing whether ther user can claim a business.
+   */
   canClaimBusiness() {
     return !this.usersBusiness;
   }
